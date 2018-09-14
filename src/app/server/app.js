04ea98c9d810f766c387a75b21e51a833c445b1d
Hazem Gharib <hazem.gharb.8@gmail.com>
2018-09-15 00:05:51 +0200

    cors

--- src/app/server/app.js
+++ src/app/server/app.js
@@ -41,21 +41,8 @@ app.use(express.static(path.join(__dirname,'..','..','..','dist','portfolio')));
 /* Configuring End */
 
 app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', process.env.CORS);
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
