5c955c9cd44da5858e003485bc5eaeb7c3542e50
Hazem Gharib <hazem.gharb.8@gmail.com>
2018-09-14 21:36:50 +0200

    Hot fix for joi dependency

--- src/app/server/routes/portfolio.js
+++ src/app/server/routes/portfolio.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 // Third party packages
-const Joi = require('Joi');
+const Joi = require('joi');
 const config = require('config');
 
 
