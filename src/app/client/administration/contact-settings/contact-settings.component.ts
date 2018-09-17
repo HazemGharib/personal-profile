d0513e171c3a4c9164dfbf6dbceba8e1de4286fb
Hazem Gharib <hazem.gharb.8@gmail.com>
2018-09-17 23:26:51 +0200

    Contact settings UI changes

--- src/app/client/administration/contact-settings/contact-settings.component.ts
+++ src/app/client/administration/contact-settings/contact-settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-contact-settings',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactSettingsComponent implements OnInit {
 
+  email = new FormControl('', [Validators.required, Validators.email]);
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  getErrorMessage() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+      this.email.hasError('email') ? 'Not a valid email' : '';
+  }
 }
