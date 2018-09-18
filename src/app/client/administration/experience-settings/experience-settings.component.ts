431d68510d2cc158a16345e22b1cd65544e153c4
Hazem Gharib <hazem.gharb.8@gmail.com>
2018-09-18 22:19:37 +0200

    Adding cards to experience settings

--- src/app/client/administration/experience-settings/experience-settings.component.ts
+++ src/app/client/administration/experience-settings/experience-settings.component.ts
@@ -7,6 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperienceSettingsComponent implements OnInit {
 
+  experience = {
+    workExperience: [
+        {
+            employerName: 'NabdaCare',
+            role: 'Dot Net Developer',
+            responsibilities: 'Lorem Ipsum',
+            isCurrent: false,
+            startDate: new Date(2018, 9, 18),
+            endDate: new Date(2018, 9, 18),
+        },
+        {
+            employerName: 'Service Rocket',
+            role: 'Agile Developer',
+            responsibilities: 'Lorem Ipsum',
+            isCurrent: true,
+            startDate: new Date(2018, 9, 18),
+            endDate: new Date(2018, 9, 18)
+        }
+    ],
+    trainingExperience: [
+        {
+            organizationName: 'Information Technology Inistitiute [ITI]',
+            field: 'Professional Developer Track [.Net Full Stack]',
+            startDate: new Date(2018, 9, 18),
+            endDate: new Date(2018, 9, 18)
+        },
+        {
+            organizationName: 'IBM Egypt',
+            field: 'Customer Relationship Management [CRM]',
+            startDate: new Date(2018, 9, 18),
+            endDate: new Date(2018, 9, 18)
+        }
+    ]
+};
+
   constructor() { }
 
   ngOnInit() {
